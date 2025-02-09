28f1996f3f1de0a7749c5d0431536e62645e3c01
gustafwingren <gustaf@gwingren.se>
2025-02-09 09:51:40 +0100

    Add prettier

--- src/frontend/eslint.config.js
+++ src/frontend/eslint.config.js
@@ -1,43 +1,64 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const unusedImports = require('eslint-plugin-unused-imports');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const simpleSort = require('eslint-plugin-simple-import-sort');
 
 module.exports = tseslint.config(
-  {
-    files: ["**/*.ts"],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
-    ],
-    processor: angular.processInlineTemplates,
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
-    },
-  },
-  {
-    files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
-    rules: {},
-  }
+	{
+		files: ['**/*.ts'],
+		plugins: {
+			'unused-imports': unusedImports,
+			'simple-import-sort': simpleSort,
+		},
+		extends: [
+			eslint.configs.recommended,
+			...tseslint.configs.recommended,
+			...tseslint.configs.stylistic,
+			...angular.configs.tsRecommended,
+			eslintPluginPrettierRecommended,
+		],
+		processor: angular.processInlineTemplates,
+		rules: {
+			'no-unused-vars': 'error',
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'error',
+			'unused-imports/no-unused-imports': 'error',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					varsIgnorePatter: '^_',
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+				},
+			],
+			'@angular-eslint/directive-selector': [
+				'error',
+				{
+					type: 'attribute',
+					prefix: 'app',
+					style: 'camelCase',
+				},
+			],
+			'@angular-eslint/component-selector': [
+				'error',
+				{
+					type: 'element',
+					prefix: 'app',
+					style: 'kebab-case',
+				},
+			],
+		},
+	},
+	{
+		files: ['**/*.html'],
+		extends: [
+			...angular.configs.templateRecommended,
+			...angular.configs.templateAccessibility,
+		],
+		rules: {},
+	},
 );
